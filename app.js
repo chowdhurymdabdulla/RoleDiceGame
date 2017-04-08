b34fb53b7c91f10c108379a92a2dfd8c7a26f28e
Abdulla Chowdhury <chowdhurymdabdulla@gmail.com>
2017-04-08 02:21:11 -0400

    update js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
  "use strict"
- var _id = (id) => document.getElementById(id);
 
 var activePlayer = true;
+var _class = (className)=> document.getElementsByClassName(className);
+var _id = (id) => document.getElementById(id);
 
 var player1 = _id("divplayer1");
 var player2 = _id("divplayer2");
@@ -11,6 +12,12 @@ var circle1 =_id("circle2");
 var circle2 =_id("circle2");
 var currentScore1 =_id("currentScore1");
 var currentScore2 =_id("currentScore2");
+var score1 = _id("score1");
+var score2 = _id("score2");
+
+var rd1 =  parseInt(die1.innerHTML);
+var rd2 =  parseInt(die2.innerHTML);
+
 
 
  function rollDice() {
@@ -24,16 +31,30 @@ var currentScore2 =_id("currentScore2");
         if(activePlayer){
             var num = currentScore1.innerHTML
             var num2 = parseInt(num);
-            var num2 = num2 + diceTotal1;
-            currentScore1.innerHTML = num2;
-            console.log(num2);
+            if( rd1 ==1 || rd2 == 1){
+                currentScore1.innerHTML = 0;
+            } else if(rd1 == 1 && rd2 == 1){
+                score1.innerHTML = 0;
+                currentScore1.innerHTML = 0;
+            }else {
+                var num2 = num2 + diceTotal1;
+                currentScore1.innerHTML = num2;
+                console.log(num2);
+            }
+           
         } else {
-                var num3 = currentScore2.innerHTML
-                var num4 = parseInt(num3);
-                var num4 = num4 + diceTotal2;
+            var num3 = currentScore2.innerHTML
+            var num4 = parseInt(num3);
+             if( rd1 == 1 || rd2 == 1){
+                currentScore2.innerHTML = 0;
+            } else if(rd1 == 1 && rd2 == 1){
+                score2.innerHTML = 0;
+                currentScore2.innerHTML = 0;
+            }else {
+                var num4 = num4 + diceTotal1;
                 currentScore2.innerHTML = num4;
+            }
         }
-
         function result(){
          if(num2>100 ||num4>100){
              alert("winner");
@@ -45,10 +66,26 @@ var currentScore2 =_id("currentScore2");
  }
 
  function holdPlayer() {
+     var cs1 =  parseInt(currentScore1.innerHTML);
+     var cs2 =  parseInt(currentScore2.innerHTML);
+     var rd1 =  parseInt(die1.innerHTML);
+     var rd2 =  parseInt(die2.innerHTML);
     if(activePlayer){
-        activePlayer = false;
-     } else
-         activePlayer = true;       
-    }  
+        var finalScore1 = parseInt(score1.innerHTML);
+            var fs1 = cs1 + finalScore1;
+            score1.innerHTML = fs1;
+            die1.innerHTML = 0;
+            die2.innerHTML = 0;
+            activePlayer = false;
+            
+     } else{
+        var finalScore2 = parseInt(score2.innerHTML);
+            var fs2 = cs2 + finalScore2;
+            score2.innerHTML = fs2;
+            die1.innerHTML = 0;
+            die2.innerHTML = 0;
+            activePlayer = true; 
+     }
+                  
+}  
   
- }
